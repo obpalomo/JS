3037c347330de364223490381f5d37cfcf985288
obpalomo <obpalomo@gmail.com>
2023-10-21 00:04:07 +0200

    Iteracion 2 finalizada

--- Js basic/iteracion_2.js
+++ Js basic/iteracion_2.js
@@ -26,7 +26,7 @@ console.log(toy1.price + toy2.price);
 
 /* // 1.4 Dado el siguiente javascript, actualiza el valor de la variable globalBasePrice a 25000
 // y actualiza la propiedad finalPrice de todos los coches con el valor de su propiedad
-// basePrice más el valor de la variable globalBasePrice. */
+// basePrice más el valor de la variable globalBasePrice. */ /* / */
 let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};

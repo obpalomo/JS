387d502a9c746dfe19ae26f2d5b0a5c638537eab
obpalomo <obpalomo@gmail.com>
2023-10-21 00:26:52 +0200

    Ejercicio 2 propuesto finalizado

--- Js basic/ejercicio_propuesto_2.js
+++ Js basic/ejercicio_propuesto_2.js
@@ -1,4 +1,4 @@
-/* 
+/*
 Las notas de María, Juan y Luis en los exámenes de evaluación son respectivamente:
 
 María: 10, 3, 6, 7
@@ -10,3 +10,31 @@ Luís: 9, 1, 1, 8
 - testea el bloque con cada alumno
 
 */
+
+let notaMaria = (10+3+6+7)/4
+let notaJuan = (5+6+1+4)/4
+let notaLuis = (9+1+1+8)/4
+
+if (notaMaria < 5) {
+    console.log('Maria', 'suspenso');
+} else if (notaMaria <7) {
+    console.log('Maria', 'aprobado');
+} else if (notaMaria >=7) {
+    console.log('Maria', 'notable');
+}
+
+if (notaJuan < 5) {
+    console.log('Juan', 'suspenso');
+} else if (notaJuan <7) {
+    console.log('Juan', 'aprobado');
+} else if (notaJuan >=7) {
+    console.log('Juan', 'notable');
+}
+
+if (notaLuis < 5) {
+    console.log('Luis', 'suspenso');
+} else if (notaLuis <7) {
+    console.log('Luis', 'aprobado');
+} else if (notaLuis >=7) {
+    console.log('Luis', 'notable');
+}

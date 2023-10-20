0cf016ea4a49a4342f7b3748291439ef2a432c7d
obpalomo <obpalomo@gmail.com>
2023-10-21 00:02:55 +0200

    Iteracion 3 finalizado

---
+++ Js basic/iteracion_3.js
@@ -0,0 +1,96 @@
+
+/* EJERCICIOS
+
+1.1 Multiplica 10 por 5 y muestra el resultado mediante console.log. */
+
+let multi = 10 * 5;
+console.log(multi);
+
+/* 1.2 Divide 10 por 2 y muestra el resultado en un console.log. */
+
+let div = 10 / 2;
+console.log(div);
+
+/* 1.3 Muestra mediante un console.log el resto de dividir 15 por 9. */
+
+let rest = 15%9;
+console.log(rest);
+
+/* 1.4 Usa el correcto operador de asignación que resultará en x = 15,
+teniendo dos variables y = 10 y z = 5. */
+
+let y=10;
+let z=5;
+let x = y + z;
+
+console.log(x);
+
+/* 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5. */
+
+x= y*z;
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+/* /*
+/* /* TEORIA CLASE */
+/* 1. OPERADORES LÓGICOS (OR, AND, NOT) */
+
+/* /* or */
+/* let llueve = true;
+let fregada = false;
+
+let isSueloMojado = llueve || fregada;
+
+console.log('mojado', isSueloMojado);  */// true
+
+
+/* and */
+/* let soyRico = false;
+let quieroUnFerrari = true;
+
+let vasATenerUnFerrari = soyRico && quieroUnFerrari;
+
+console.log('tengo', vasATenerUnFerrari); */ /* */
+
+/* not */
+
+/* let tengoDinero = false;
+
+if (tengoDinero) {
+    console.log('no ir al cajero');
+} else {
+    console.log('ir al cajero'); */
+/* } */
+
+/* !true -> false
+!false -> true */
+
+/* 2. OPERADORES ARITMÉTICOS (+, -, *, /, %, ++, --) */
+
+// let x = 15
+// let y = 4
+
+// aritmetica
+/* let suma = x + y;
+let resta = x - y;
+let multi = x * y;
+let divi = x / y;
+let modulo = x%y; */
+
+/* pre-incremento ++x -> se incrementa la variable y luego se asigna */
+
+/* let pre = ++x; // pre = 16, x = 16
+
+/* post-incremento x++ -> se asigna la variable y luego se incrementa */
+
+// let post = y++; // pre = 4, y = 5 */ */ */ */
